8cd94a50388167b7322bb6365082255c1aa9e3f7
Ishan Koul <ishanrajkoul@gmail.com>
2018-07-25 15:03:48 +0400

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,9 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+       return console.log('Unable to connect to mongodb server.');
+    }
+    console.log('Connected to mongodb server');
+    db.close();
+});
