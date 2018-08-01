9d977dff92da88ff7ed45fdbf8244f7595f60a8d
Ishan Koul <ishanrajkoul@gmail.com>
2018-08-01 17:31:58 +0400

    add get todos

--- server/server.js
+++ server/server.js
@@ -9,14 +9,21 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res)=>{
-    // console.log(req.body);
+app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
     });
 
     todo.save().then((doc) => {
         res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((doc) => {
+        res.send(doc)
     }, (e) => {
         res.status(400).send(e);
     });
